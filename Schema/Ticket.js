2c3df7128a4675f9246dfaf768a844e5dbbef7af
Akshay Pratap Singh <akshay124-pixel@example.com>
2024-12-21 12:31:39 +0530

    Updated

--- Schema/Ticket.js
+++ Schema/Ticket.js
@@ -4,7 +4,7 @@ const TicketSchema = new mongoose.Schema(
   {
     customerName: { type: String, required: true },
     organization: { type: String },
-    serialNumber: { type: String, default: "N/A" },
+    serialNumber: { type: String },
     description: { type: String, required: true },
     email: { type: String, required: true },
     contactNumber: { type: Number, required: true },
